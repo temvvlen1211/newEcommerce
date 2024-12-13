2c3d4e3685e4bf143ed004befc5a3d38869184cc
Temuulen <temvvlen1211@gmail.com>
2024-12-13 12:55:12 +0800

    barbercrop footer start

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,22 +1,21 @@
+import Logo from "../svg/logo";
+
 function Footer() {
   return (
     <>
-      <footer class="bg-white dark:bg-gray-900">
-        <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+      <footer class=" flex justify-center items-center bg-[#202020]  dark:bg-gray-900">
+        <div class=" w-full container  lg:py-8">
           <div class="md:flex md:justify-between">
-            <div class="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" class="flex items-center">
-                <img
-                  src="https://flowbite.com/docs/images/logo.svg"
-                  class="h-8 me-3"
-                  alt="FlowBite Logo"
-                />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                  Flowbite
-                </span>
-              </a>
-            </div>
-            <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+            <a
+              href=""
+              className="flex items-center space-x-3 rtl:strokeLinejoin"
+            >
+              <Logo />
+              <span className="self-center text-2xl font-semibold uppercase whitespace-nowrap dark:text-white">
+                barbercrop
+              </span>
+            </a>
+            <div class="flex ">
               <div>
                 <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
                   Resources
@@ -53,23 +52,6 @@ function Footer() {
                       class="hover:underline"
                     >
                       Discord
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Legal
-                </h2>
-                <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                  <li class="mb-4">
-                    <a href="#" class="hover:underline">
-                      Privacy Policy
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="hover:underline">
-                      Terms &amp; Conditions
                     </a>
                   </li>
                 </ul>
