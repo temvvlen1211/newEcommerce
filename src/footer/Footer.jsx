d6445d233255eada747001985e84f471145ef4a8
Temuulen <temvvlen1211@gmail.com>
2024-12-17 21:19:08 +0800

    mini change

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -62,7 +62,7 @@ function Footer() {
           <div class="sm:flex sm:items-center sm:justify-between">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
               {new Date().getFullYear()}
-              <a href="https://flowbite.com/" class="hover:underline">
+              <a href="" class="hover:underline">
                 Flowbite™
               </a>
               . All Rights Reserved.
